4fd5c897fa97092825a243b6233aab06027a3cd1
HuzaifaAslam99 <huzaifaaslam322@gmail.com>
2024-08-15 14:21:26 -0700

    changes

--- new2.js
+++ new2.js
@@ -192,8 +192,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
 });
 
 async function getSongs() {
-    // let a = await fetch("/songs/");
-    let a = await fetch("http://127.0.0.1:51422/songs");
+    let a = await fetch("/songs/");
+    // let a = await fetch("http://127.0.0.1:51422/songs");
     let response = await a.text(); 
     console.log(response);
     
